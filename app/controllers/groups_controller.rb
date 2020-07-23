37a47f56f7caf9c84b1f6d06597bf654ca4aad87
DevNeoLee <devneolee@gmail.com>
2020-07-23 06:06:19 -0700

    create main triple complex form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,35 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @groups = Group.all.reverse
+    @albums = Album.all.reverse
+    @songs = Song.all.reverse 
   end
 
   def new
+    @group = Group.new
+    @group.albums.build.songs.build 
+  end
+
+  def create
+    group = Group.new(group_params)
+
+    if group.save! 
+      redirect_to root_path
+    else 
+      redirect_to new_group_path
+    end
+  end
+
+  def destroy
+    Group.destroy(group_params[:group_id])
+
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :profile, albums_attributes: [:title, :release_date, songs_attributes: [:title, :korean_lyrics, :romaja_lyrics]])
   end
 end
