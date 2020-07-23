a504fd53e2f52e9a61c9a32ee82d5a1837d24504
DevNeoLee <devneolee@gmail.com>
2020-07-22 19:07:13 -0700

    initial model before migration

---
+++ db/migrate/20200723020333_create_translations.rb
@@ -0,0 +1,14 @@
+class CreateTranslations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :translations do |t|
+      t.string :title
+      t.text :content
+      t.string :language
+      t.integer :like
+      t.references :song, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
