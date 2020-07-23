14935cb31d72a72fdca5dc185ec8999c165431f1
DevNeoLee <devneolee@gmail.com>
2020-07-22 23:53:21 -0700

    create admin user boolean default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_020508) do
+ActiveRecord::Schema.define(version: 2020_07_23_064327) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_020508) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
