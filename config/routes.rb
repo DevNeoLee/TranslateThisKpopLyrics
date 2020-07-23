f3352fd16347f32658538e80e3b65fa52f4bdafc
DevNeoLee <devneolee@gmail.com>
2020-07-22 17:41:22 -0700

    create user model devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'groups/index'
+  get 'groups/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'groups#index'
+
+  resources :groups
 end
