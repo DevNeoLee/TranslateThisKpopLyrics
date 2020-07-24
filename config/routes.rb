ffc0e4b5aaac31f54cf50f1fd0c698ff2a31fa77
DevNeoLee <devneolee@gmail.com>
2020-07-23 21:07:31 -0700

    add nested attributes independent delete edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'albums/index'
+  get 'songs/index'
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks"}
 
-  get 'groups/index'
-  get 'groups/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'groups#index'
 
-  resources :groups
+  resources :groups 
+  resources :albums, only: [:edit, :update, :destroy]
+  resources :songs, only: [:edit, :update, :destroy]
+  # resources :songs  
+
 end
